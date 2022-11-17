cce39cd214c2386b45f748e56684739c183d61b1
apran42 <flyingdisc1@naver.com>
2022-11-17 15:03:19 +0900

    도서관 관리 파일

---
+++ teamproject/library/LibraryManager.java
@@ -0,0 +1,63 @@
+package teamproject.library;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LibraryManager {
+
+    ArrayList<Object> list = new ArrayList<>();
+
+    LibraryManager(String path) {
+        File file = new File(path);
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            int i=0;
+            while((line = br.readLine()) != null) {
+                if(i < 2) {
+                    list.add(Integer.parseInt(line));
+                    i++;
+                }
+                else {
+                    String a = line.split(" ")[0];
+                    String b = line.split(" ")[1];
+                    int c = Integer.parseInt(line.split(" ")[2]);
+                    list.add(a);
+                    list.add(b);
+                    list.add(c);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void start(Scanner sc) {
+        String start = sc.nextLine();
+        if(start.equals("")) {
+            System.out.println("# DMU 도서실 좌석 관리 시스템 #");
+            System.out.println("############ 메 뉴 ############");
+            System.out.println("\t0. 종\t료");
+            System.out.println("\t1. 입\t실");
+            System.out.println("\t2. 퇴\t실");
+            System.out.println("\t3. 정기 이용권");
+            System.out.println("\t4. 정기이용 탈퇴");
+            System.out.println("###############################");
+            int num = sc.nextInt();
+            switch (num) {
+                case 0: Program.exit(); break;
+                case 1: Program.check_In(); break;
+                case 2: Program.check_Out(); break;
+                case 3: Program.season_Pass(); break;
+                case 4: Program.season_Pass_Withdraw(); break;
+                default:
+                    System.out.println("잘못된 번호를 입력하였습니다.");
+                    System.out.println("다시 올바른 번호를 입력해주세요.");
+                    start(sc); break;
+            }
+        }
+    }
+}
