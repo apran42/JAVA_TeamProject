c791f5d37f611b55384706e7fda0acdd3881009c
apran42 <flyingdisc1@naver.com>
2022-11-22 09:50:47 +0900

    Update program.java

--- teamproject/library/program.java
+++ teamproject/library/program.java
@@ -2,24 +2,23 @@ package teamproject.library;
 
 import java.util.Scanner;
 
-class Program {
-    // 0. 퇴실
-    static void exit() {
+public class Program extends SeosonPassSale{
+    // 0. 종료
+    public void exit() {
         System.exit(0);
     }
     // 1. 입실
-    static void check_In() {
+    public void check_In() {
         Scanner sc = new Scanner(System.in);
         System.out.println();
         System.out.print("--> 정기 이용 회원인가요? (Y/N) ");
         String is_mem = sc.next();
-        System.out.println();
         
         // 1-1. 정기회원인 경우 
         if(is_mem.equals("y") || is_mem.equals("Y")) {
             System.out.println();
             System.out.println("--> 회원번호를 입력하세요");
-            int no = sc.nextInt();
+            int no = sc.nextInt();            
         }
 
         // 1-2. 정기회원이 아닌 경우 
@@ -27,7 +26,7 @@ class Program {
             System.out.println();
 
             System.out.print("--> 학생인가요? (Y/N) ");
-            String is_student = sc.next();
+            String student = sc.next();
             System.out.println();
 
             System.out.print("--> 이름 입력 : ");
@@ -35,25 +34,45 @@ class Program {
             System.out.println();
 
             System.out.print("--> 전화번호 입력: ");
-            int tel = sc.nextInt();
+            String tel = sc.nextLine();
             System.out.println();
-
-            System.out.println(is_student + "\t\t" + name);
-            System.out.println(tel);
 
         }
 
     }
 
-    static void check_Out() {
+    // 2. 퇴실
+    public void check_Out() {
         
     }
 
-    static void season_Pass() {
-        System.out.println(3);
+    // 3. 정기 이용권
+    public void season_Pass() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+
+        System.out.print("--> 학생인가요? (Y/N) ");
+        String stu = sc.nextLine();
+        System.out.println();
+        isStudent(stu);
+
+        System.out.print("--> 이름 입력 : ");
+        String name = sc.nextLine();
+        System.out.println();
+        setName(name);
+
+        System.out.print("--> 전화번호 입력 :  ");
+        String tele = sc.nextLine();
+        System.out.println();
+        setTele(tele);
+
+        super.register();
+        
+        sc.close();
     }
 
-    static void season_Pass_Withdraw() {
+    //4.정기이용 탈퇴
+    public void season_Pass_Withdraw() {
         System.out.println(4);
     }
 
