54b2790acf8ca5802e66399561fffb8e90119840
apran42 <107315509+apran42@users.noreply.github.com>
2022-11-30 05:15:21 +0900

    이전의 코드 삭제
    더 이상 필요없어진 코드 삭제함

--- teamproject/library/CheckSeat.java
+++
@@ -1,9 +0,0 @@
-package teamproject.library;
-
-public class CheckSeat{
-
-    public String isStudent(int no) {
-        SeasonPassSale sps = new SeasonPassSale();
-        return (String)sps.getElement((no - 1)*5 + 1);
-    }
-}
